2c3aebd9b4571088152b7d50539253fa55d147b3
Rakesh Basnet <rakeshbasnet086@gmail.com>
2025-03-18 14:29:51 -0400

    updating the public access setting of S3 bucket

--- main.tf
+++ main.tf
@@ -2,3 +2,28 @@ resource "aws_s3_bucket" "bucket" {
   bucket = var.bucketName
 }
 
+resource "aws_s3_bucket_ownership_controls" "bucket_owner" {
+  bucket = aws_s3_bucket.bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket = aws_s3_bucket.bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "bucket_acl" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.bucket_owner,
+    aws_s3_bucket_public_access_block.public_access,
+  ]
+
+  bucket = aws_s3_bucket.bucket.id
+  acl    = "public-read"
+}
